b39869b687193424750d5b3fa76bd159f9ee9107
Chris Harding <wablahflababablah@gmail.com>
2015-10-19 03:23:50 +0000

    Initial

---
+++ index.js
@@ -0,0 +1,15 @@
+setTimeout(function() {
+  var d = document.getElementById("title")
+  d.style.opacity = 1
+}, 500);
+
+setTimeout(function() {
+  var d = document.getElementById("motto")
+  d.style.opacity = 1
+}, 2000);
+
+setTimeout(function() {
+  var d = document.getElementById("salty")
+  d.style.opacity = 1
+}, 3500);
+
